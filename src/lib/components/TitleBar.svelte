c6aabdcb32cdfa21b80eb5453c5fa073322d6523
gaconkzk <gaconkzk@gmail.com>
2022-08-21 06:59:27 +0700

    transparent background for rounded top border

--- src/lib/components/TitleBar.svelte
+++ src/lib/components/TitleBar.svelte
@@ -6,13 +6,29 @@
   const { class: containerClass, ...restProps } = $$props
 
   let isTauri = false
+  let maximized = false
+
+  const toggleMaximize = async () => {
+    try {
+      await appWindow.toggleMaximize()
+      maximized = !maximized
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
   onMount(() => {
     isTauri = !!window.__TAURI__
   })
 </script>
 
 {#if isTauri}
-  <div data-tauri-drag-region class="titlebar {containerClass}" {...restProps}>
+  <div
+    data-tauri-drag-region
+    class="titlebar {containerClass}"
+    class:titlebar-rounded={!maximized}
+    {...restProps}
+  >
     <TitleBarButton
       id="titlebar-minimize"
       src="https://api.iconify.design/mdi:window-minimize.svg"
@@ -23,7 +39,7 @@
       id="titlebar-minimize"
       src="https://api.iconify.design/mdi:window-maximize.svg"
       alt="maximize"
-      on:click={() => appWindow.toggleMaximize()}
+      on:click={() => toggleMaximize()}
     />
     <TitleBarButton
       id="titlebar-close"
@@ -36,6 +52,9 @@
 
 <style>
   .titlebar {
-    @apply fixed h-30px top-0 left-0 right-0 select-none flex justify-end bg-cyan-300 rounded;
+    @apply fixed h-30px top-0 left-0 right-0 select-none flex justify-end bg-cyan-300;
+  }
+  .titlebar-rounded {
+    @apply rounded-t-2xl;
   }
 </style>
